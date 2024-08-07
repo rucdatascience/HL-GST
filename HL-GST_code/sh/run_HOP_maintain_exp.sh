9321fff809945aaff76fe94eba898a688b13bd2f
rucdatascience <87228246+rucdatascience@users.noreply.github.com>
2024-08-07 18:50:28 +0800

    1

--- HL-GST_code/sh/run_HOP_maintain_exp.sh
+++ HL-GST_code/sh/run_HOP_maintain_exp.sh
@@ -1,8 +1,8 @@
 g++ -std=c++17 -I/home/boost_1_75_0 -I/root/HL-GST_code/include /root/HL-GST_code/src/HOP_maintain/exp_HL4GST_HOP_maintain.cpp -lpthread -O3 -o A
-./A musae musae 300 600&
-./A twitch twitch 300 600&
-./A github github 300 600&
-./A amazon amazon 300 600&
-./A reddit reddit 300 600&
+./A musae musae 300 600
+./A twitch twitch 300 600
+./A github github 300 600
+./A amazon amazon 300 600
+./A reddit reddit 300 600
 ./A dblp dblp 300 600
 rm A
